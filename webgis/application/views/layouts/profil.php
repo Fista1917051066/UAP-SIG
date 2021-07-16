06443dcb2b3b000ed48976c062a9312598fb5487
Viny Silvia <62374456+VinySilvia@users.noreply.github.com>
2021-07-15 18:02:52 -1200

    Update profil.php

---
+++ webgis/application/views/layouts/profil.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>About Us</title>
+  </head>
+  
+  <body>  
+    <center>
+    <div class="container" >
+        <div class="row" >
+            <div class="container" >
+                <div class="row" >
+                    <div class="col-sm-3 border border-dark bg-light text-black rounded" >
+                        ​<picture>
+                            <img src="<?=base_url('Viny.jpg')?>"  class="rounded-circle border border-dark " width= "100px">
+                        </picture>
+                        <h6><div class= "font-weight-bold" >
+                            <br>1917051010<br>
+                            Viny Silvia</h6>   
+                            <a class="border border-dark btn-dark text-white" href="https://instagram.com/vinysilvia__?utm_medium=copy_link" role="button">Follow Us</a>           
+                    </div>
+                    <div class="col-sm-3 border border-dark bg-light text-clack rounded" >
+                        ​<picture>
+                            <img src="<?=base_url('Mita.jpeg')?>"  class="rounded-circle border border-dark " width= "100px">
+                        </picture>
+                        <h6><div class= "font-weight-bold" >
+                            <br>1917051040<br>
+                            Mita Anggraeni</h6>   
+                            <a class="border border-dark btn-dark text-white" href="https://instagram.com/mita_anggraenni?utm_medium=copy_link" role="button">Follow Us</a>           
+                    </div>
+                
+
+                <div class="col-sm-3 border border-dark bg-light text-black rounded" >
+                        <picture>
+                            <img src="<?=base_url('Della.png')?>"  class="rounded-circle border border-dark " width= "100px">
+                        </picture>
+                        <h6><div class= "font-weight-bold" >
+                            <br>1917051056<br>
+                            Della Hayu Enggarini</h6>
+                            <a class="border border-dark btn-dark text-white" href=https://instagram.com/dellahayue_?utm_medium=copy_link role="button">Follow Us</a>
+                    </div>
+                    
+
+                    <div class="col-sm-3 border border-dark bg-light text-black rounded"  >
+                    ​<picture>
+                    <img src="<?=base_url('Fista.jpeg')?>"  class="rounded-circle border border-dark " width= "100px">
+                    </picture>
+                    <h6><div class= "font-weight-bold" >
+                        <br>1917051066<br>
+                        Fista Dwi Septiana</h6>
+                        <a class="border border-dark btn-dark text-white" href=https://instagram.com/fista.dwis?utm_medium=copy_link role="button">Follow Us</a>
+                    </div>
+                </div>
+                </div>
+       </div>
+  </div>
+    
+                
+
+    
+</div>
+
+</center>
